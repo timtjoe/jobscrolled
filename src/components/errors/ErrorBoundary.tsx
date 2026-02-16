ea8ce30ffd6609a2ac91b55b1b190b34e98250d3
timtjoe <ttjoe0@gmail.com>
2026-02-16 02:12:02 +0000

    fix: remove unused import

--- src/components/errors/ErrorBoundary.tsx
+++ src/components/errors/ErrorBoundary.tsx
@@ -1,4 +1,4 @@
-import React, { Component, type ErrorInfo, type ReactNode } from "react";
+import { Component, type ErrorInfo, type ReactNode } from "react";
 
 interface Props {
   /** The UI that might crash */
